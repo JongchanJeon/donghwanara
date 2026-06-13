e4db5e4eb67ccd46d291954642b5d30a4499f0b7
Jongchan Jeon <103109563+JongchanJeon@users.noreply.github.com>
2026-06-13 10:51:44 +0900

    [chore] 도메인 연결 설정

--- vite.config.ts
+++ vite.config.ts
@@ -16,5 +16,12 @@ export default defineConfig({
     server: {
       port: 5173,
     },
+    
+    preview: {
+      host: "0.0.0.0",
+      port: 6060,
+      strictPort: true,
+      allowedHosts: ["donghwanara.site"],
+    },
   },
 });
